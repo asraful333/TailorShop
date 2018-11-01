f96cbcdd0b276122926de9bd42a4cd41ad3c8b46
asraful333 <asrafulkarim333@gmail.com>
2018-11-01 10:30:21 +0600

    Add order change remove reepeter

--- add_order.php
+++ add_order.php
@@ -39,19 +39,88 @@
 				                    <th>Amount</th>
 				                    <th>Fabric</th>
 				                    <th>Master</th>
-				                    <th><button type="button" name="add" class="btn btn-success add" >Add Another</button></th>
+				                    <th>Action</th>
 				                </tr>
 				                <tr>
-				                    <td><select name="service[]" class="form-control service"><option>Select Service</option><option>Shirt</option><option>Pant</option><option>Suit</option></select></td>
+				                    <td><select name="service[]" class="form-control service">
+				                    	<option>Select Service</option>
+				                    	<option value="shirt">Shirt</option>
+				                    	<option value="pant">Pant</option>
+				                    	<option value="suit">Suit</option>
+				                    </select></td>
 				                    <td><input type="text" name="rate[]" class="form-control rate" /></td>
 				                    <td><input type="text" name="quantity[]" class="form-control quantity" /></td>
 				                    <td><input type="text" name="amount[]" class="form-control amount" /></td>
 				                    <td><input type="text" name="fabric[]" class="form-control fabric" /></td>
 				                    <td><select name="master[]" class="form-control master"><option>Select master</option><option>Nayem</option><option>Shimul</option><option>Sumon</option></select></td>
+				                    <td><button type="button" name="add" class="btn btn-success" >Add</button></td>
 				                </tr>
 				            </table>
+
+				            <div class="col-md-4">
+					            <div class="optional shirt form-group" style="display: none;">
+					            	<h1>Measurement</h1>
+					            	<label class="form-control">Body</label>
+					            	<input type="text" class="form-control" name="body" />
+					            </div>					            				            	
+
+
+
+					            <div class="optional pant form-group" style="display: none;">
+					            	<h1>Measurement</h1>
+					            	<label class="form-control">Length</label>
+					            	<input type="text" class="form-control" name="length" />
+					            </div>
+
+
+					            <div class="optional suit form-group" style="display: none;">
+					            	<h1>Measurement</h1>
+					            	<label class="form-control">chist</label>
+					            	<input type="text" class="form-control" name="chist" />
+					            </div>
+
+				            </div>
 				        </div>
-				        
+				        <div class="table-responsive">
+					            <table class="table table-bordered">
+					            	<h3>Sub-Orders</h3>
+					            	<tr>
+						            	<th>Order Id</th>
+						            	<th>Customer Name</th>
+						            	<th>Service</th>
+						            	<th>Quantity</th>
+						            	<th>Master</th>
+					            	</tr>
+					            	<tr>
+					            		<td>1</td>
+					            		<td>Nayem</td>
+					            		<td>Shirt</td>
+					            		<td>2</td>
+					            		<td>Shimul</td>
+					            	</tr>
+					            </table>
+				            </div>
+				            
+						    <div class="col-md-4">
+							    <div class="input-group">
+							      <span class="input-group-addon">Total Amount</span>
+							      <input id="msg" type="text" class="form-control" name="msg" placeholder="Total">
+							    </div>
+						    </div>
+						    <div class="col-md-4">
+							    <div class="input-group">
+							      <span class="input-group-addon">Discount</span>
+							      <input id="msg" type="text" class="form-control" name="msg" placeholder="Discount">
+							    </div>
+						    </div>
+						    <div class="col-md-4">
+							    <div class="input-group">
+							      <span class="input-group-addon">Due</span>
+							      <input id="msg" type="text" class="form-control" name="msg" placeholder="Due">
+							    </div>
+						    </div>
+						   			
+							<center><a type="button" class="btn btn-success" href="" style="margin-top: 20px;">Submit Order</a></center>
 				</div>
 		    </form>
 		</div>
