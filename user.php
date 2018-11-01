f58e4a07546c7dd5179a552fbadc5907e6a53410
asraful333 <asrafulkarim333@gmail.com>
2018-11-01 10:37:21 +0600

    Update

--- user.php
+++ user.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-	<head>
-		<title>Tailor Shop MS</title>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-		<!-- VENDOR CSS -->
-		<link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
-		<link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
-		<link rel="stylesheet" href="assets/vendor/linearicons/style.css">
-		<link rel="stylesheet" href="assets/vendor/chartist/css/chartist-custom.css">
-		<!-- MAIN CSS -->
-		<link rel="stylesheet" href="assets/css/main.css">
-		<!-- GOOGLE FONTS -->
-		<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
-		<!-- ICONS -->
-		<link rel="icon" type="image/png" sizes="96x96" href="assets/img/sewing.png">
-	</head>
+<?php include 'inc/headerplugin.php'; ?>
 
 	<body>
 		<div id="wrapper">
@@ -97,47 +78,4 @@
 			</footer>
 		</div>
 
-		<script src="assets/vendor/jquery/jquery.min.js"></script>
-		<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-		<script src="assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-		<script src="assets/vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
-		<script src="assets/vendor/chartist/js/chartist.min.js"></script>
-		<script src="assets/scripts/klorofil-common.js"></script>
-	    <!-- Import repeater js  -->
-	    <script src="assets/vendor/jquery/repeater.js" type="text/javascript"></script>
-	    <!--Repeter js-->
-		<script type="text/javascript">
-		    $(document).ready(function(){
-		        $(document).on('click','.add',function(){
-		            var html = '';
-		            html += '<tr>';
-		            html += '<td><select name="service[]" class="form-control service"><option>Select Service</option><option>Shirt</option><option>Pant</option><option>Suit</option></select></td>';
-		            html += '<td><input type="text" name="rate[]" class="form-control rate" /></td>';
-		            html += '<td><input type="text" name="quantity[]" class="form-control quantity" /></td>';
-		            html += '<td><input type="text" name="amount[]" class="form-control amount" /></td>';
-		            html += '<td><input type="text" name="fabric[]" class="form-control fabric" /></td>';
-		            html += '<td><select name="master[]" class="form-control master"><option>Select master</option><option>Nayem</option><option>Shimul</option><option>Sumon</option></select></td>';
-		            html += '<td><button type="button" name="remove" class="btn btn-success remove" >Remove</button></td>';
-		            $('#item_table').append(html);
-		        });
-		        $(document).on('click','.remove',function(){
-		            $(this).closest('tr').remove();
-		        });
-		    });
-		</script>
-		<!--End Repeter-->
-	    <!--Filter-->
-		<script>
-			$(document).ready(function(){
-			  $("#myInput").on("keyup", function() {
-			    var value = $(this).val().toLowerCase();
-			    $("#myTable tr").filter(function() {
-			      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-			    });
-			  });
-			});
-		</script>
-		<!--DateTime Picker-->
-
-	</body>
-</html>
+<?php include 'inc/footerplugin.php'; ?>
