1464e605db063696ed205d0f1329965dc5108343
asraful333 <asrafulkarim333@gmail.com>
2018-11-05 12:35:10 +0600

    Insert data into database create database connection for customers

--- add_customer.php
+++ add_customer.php
@@ -1,3 +1,35 @@
+<?php
+	include 'inc/connect.php';
+
+	if (isset($_POST['submit'])) {
+
+		$name = mysqli_real_escape_string($conn,$_POST['name']);
+		$age = mysqli_real_escape_string($conn,$_POST['age']);
+		$phone = mysqli_real_escape_string($conn,$_POST['phone']);
+		$address = mysqli_real_escape_string($conn,$_POST['address']);
+		$address = mysqli_real_escape_string($conn,$_POST['address']);
+		$gender = mysqli_real_escape_string($conn,$_POST['gender']);
+
+		$check = "SELECT * FROM customer_tb WHERE customer_phone ='$phone'";
+		$res_c = mysqli_query($conn,$check) or die(mysqli_error($conn));
+
+		if (mysqli_num_rows($res_c)>0) {
+			$msg= "Phone number is already exists";
+		}
+		else{
+
+			$qr = "INSERT INTO `customer_tb`(`customer_name`, `customer_age`, `customer_phone`,`customer_address`,`customer_gender`) VALUES ('$name','$age','$phone','$address','$gender')";
+
+			$qry = mysqli_query($conn,$qr);
+			if ($qry) {
+				header('Location: user.php?page=customers_list');
+			}else{
+				echo "Insert Failed!..";
+			}
+		}
+	}
+?>
+
 <div class="panel panel-default">
 					<div class="panel-heading">
 						<h3>Add Customer</h3>
@@ -5,43 +37,46 @@
 					<div class="panel-body">
 						<div style="max-width: 600px; margin: 0 auto;">
 
-							<form class="form-horizontal" action="" method="">
+							<form class="form-horizontal" action="" method="POST">
 								
 								  <div class="form-group required">
 								    <label class="control-label col-sm-2" for="name">Name:</label>
 								    <div class="col-sm-10">
-								      <input type="text" class="form-control" id="name" placeholder="Enter Name">
+								      <input type="text" class="form-control" name="name" id="name" placeholder="Enter Name">
 								    </div>
 								  </div>
 								  <div class="form-group required">
 								    <label class="control-label col-sm-2" for="pwd">Age:</label>
 								    <div class="col-sm-10"> 
-								      <input type="text" class="form-control" id="age" placeholder="Enter Age">
+								      <input type="text" class="form-control" name="age" id="age" placeholder="Enter Age">
 								    </div>
 								  </div>
 								  <div class="form-group required">
 								    <label class="control-label col-sm-2" for="pwd">Phone:</label>
 								    <div class="col-sm-10"> 
-								      <input type="text" class="form-control" id="phone" placeholder="Enter Phone">
+								      <input type="text" class="form-control" name="phone" id="phone" placeholder="Enter Phone">
 								    </div>
+								    <?php if (isset($msg)): ?>
+										<span><?php echo $msg;?></span>
+									<?php endif ?>
 								  </div>
 								  <div class="form-group required">
-								    <label class="control-label col-sm-2" for="pwd">Adderss:</label>
+								    <label class="control-label col-sm-2" for="pwd">Address:</label>
 								    <div class="col-sm-10"> 
-								      <textarea class="form-control" rows="4" id="address" placeholder="Enter Address"></textarea>
+								      <textarea class="form-control" rows="4" name="address" id="address" placeholder="Enter Address"></textarea>
 								    </div>
 								  </div>
 								  <div class="form-group required">
 								    <label class="control-label col-sm-2" for="pwd">Gender:</label>
 								    <div class="col-sm-10"> 
-								      <label class="radio-inline"><input type="radio" name="gender" checked>Male</label>
-									  <label class="radio-inline"><input type="radio" name="gender">Female</label>
-									  <label class="radio-inline"><input type="radio" name="gender">Other</label>
+								      <label class="radio-inline"><input type="radio" name="gender" value="Male" checked>Male</label>
+									  <label class="radio-inline"><input type="radio" name="gender" value="Female">Female</label>
+									  <label class="radio-inline"><input type="radio" name="gender" value="Other">Other</label>
 								    </div>
 								  </div>							  
 								  <div class="form-group"> 
 								    <div class="col-sm-offset-2 col-sm-10">
-								      <button type="submit" class="btn btn-success">Submit</button>
+								      <button type="submit" name="submit" class="btn btn-success">Submit</button>
 								    </div>
 								  </div>
 							</form>
