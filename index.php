d44c86bd01baa1e5cabb8089fa655f76d2c9ad04
asraful333 <asrafulkarim333@gmail.com>
2018-11-05 10:48:22 +0600

    login page

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+	include 'inc/headerplugin.php';
+?>
+
+<body>
+	<!-- WRAPPER -->
+	<div id="wrapper" style="margin-top: 90px;">
+		<div class="vertical-align-wrap">
+			<div class="vertical-align-middle">
+				<div class="auth-box ">
+					<div class="left">
+						<div class="content">
+							<div class="header">
+								<div class="logo text-center"><img src="assets/img/sewing.png" alt="Klorofil Logo" style="width: 100px; height: 60px;"> <h1>Tailor Shop</h1></div>
+								<p class="lead">Login to your account</p>
+							</div>
+							<form class="form-auth-small" action="">
+								<div class="form-group">
+									<label for="signin-username" class="control-label sr-only">Username</label>
+									<input type="text" class="form-control" id="username" name="username" placeholder="Username">
+								</div>
+								<div class="form-group">
+									<label for="signin-password" class="control-label sr-only">Password</label>
+									<input type="password" class="form-control" id="password" name="password" placeholder="Password">
+								</div>
+								<div class="form-group clearfix">
+									<label class="fancy-checkbox element-left">
+										<input type="checkbox">
+										<span>Remember me</span>
+									</label>
+								</div>
+								<button type="submit" class="btn btn-primary btn-lg btn-block">LOGIN</button>
+								<div class="bottom">
+									<span class="helper-text"><i class="fa fa-lock"></i> <a href="#">Forgot password?</a></span>
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class="right">
+						<div class="overlay"></div>
+						<div class="content text">
+							<h1 class="heading">Free Bootstrap dashboard template</h1>
+							<p>by The Develovers</p>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- END WRAPPER -->
+</body>
+
+</html>
