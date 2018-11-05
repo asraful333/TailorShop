c4bb5578e8b596a6fa2799365913b5f94b8a97ed
asraful333 <asrafulkarim333@gmail.com>
2018-11-05 10:43:54 +0600

    system user and master add form

---
+++ add_staff.php
@@ -0,0 +1,61 @@
+<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3>Add Staff</h3>
+					</div>
+					<div class="panel-body">
+						<div style="max-width: 600px; margin: 0 auto;">
+
+							<form class="form-horizontal" action="" method="">
+								
+								  <div class="form-group required">
+								    <label class="control-label col-sm-3" for="name">Name:</label>
+								    <div class="col-sm-9">
+								      <input type="text" class="form-control" id="name" placeholder="Enter Name">
+								    </div>
+								  </div>
+								  <div class="form-group required">
+								    <label class="control-label col-sm-3" for="pwd">Age:</label>
+								    <div class="col-sm-9"> 
+								      <input type="text" class="form-control" id="age" placeholder="Enter Age">
+								    </div>
+								  </div>
+								  <div class="form-group required">
+								    <label class="control-label col-sm-3" for="pwd">Phone:</label>
+								    <div class="col-sm-9"> 
+								      <input type="text" class="form-control" id="phone" placeholder="Enter Phone">
+								    </div>
+								  </div>
+								  <div class="form-group required">
+								    <label class="control-label col-sm-3" for="pwd">Adderss:</label>
+								    <div class="col-sm-9"> 
+								      <textarea class="form-control" rows="4" id="address" placeholder="Enter Address"></textarea>
+								    </div>
+								  </div>
+								  <div class="form-group required">
+								    <label class="control-label col-sm-3" for="pwd">Gender:</label>
+								    <div class="col-sm-9"> 
+								      <label class="radio-inline"><input type="radio" name="gender" checked>Male</label>
+									  <label class="radio-inline"><input type="radio" name="gender">Female</label>
+									  <label class="radio-inline"><input type="radio" name="gender">Other</label>
+								    </div>
+								  </div>
+								  <div class="form-group required">
+								    <label class="control-label col-sm-3" for="pwd">Staff Type:</label>
+								    <div class="col-sm-9"> 
+										<select class="form-control" id="sel1">
+											<option readonly>Select</option>
+											<option>Syetem User</option>
+											<option>Master</option>
+										</select>
+								    </div>
+								  </div>						  
+								  <div class="form-group"> 
+								    <div class="col-sm-offset-3 col-sm-9">
+								      <button type="submit" class="btn btn-success">Submit</button>
+								    </div>
+								  </div>
+							</form>
+
+						</div>
+					</div>
+				</div>
