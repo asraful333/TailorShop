9bd8db61a3a51e94a8ec167ebf9207ee6866df0d
asraful333 <asrafulkarim333@gmail.com>
2018-11-05 10:53:05 +0600

    view system user and master list

---
+++ staff_list.php
@@ -0,0 +1,53 @@
+<!-- TABLE STRIPED -->
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3>Staff List</h3>
+	</div>
+	<div class="panel-body">
+		<input class="form-control pull-right" id="myInput" type="text" placeholder="Search.." style="width: 50%;">
+		<br><br>
+		<table class="table table-hover">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Name</th>
+					<th>Age</th>
+					<th>Phone</th>
+					<th>Address</th>
+					<th>Staff Type</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody id="myTable">
+				<tr>
+					<td>1</td>
+					<td>Asraful Karim Nayem</td>
+					<td>24</td>
+					<td>01676728073</td>
+					<td>Uttara, Dhaka-1230</td>
+					<td>System User</td>
+					<td><a class="btn btn-success" href="#">Edit</a></td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>Sumon Hossain</td>
+					<td>23</td>
+					<td>01676728071</td>
+					<td>Uttara, Dhaka-1230</td>
+					<td>Master</td>
+					<td><a class="btn btn-success" href="#">Edit</a></td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td>Kamran Hasan</td>
+					<td>23</td>
+					<td>01776728075</td>
+					<td>Uttara, Dhaka-1230</td>
+					<td>Master</td>
+					<td><a class="btn btn-success" href="#">Edit</a></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+<!-- END TABLE STRIPED -->
