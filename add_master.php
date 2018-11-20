13c99c95b897159cb919afe235b7f4a3d7aa8a5e
asraful333 <asrafulkarim333@gmail.com>
2018-11-20 11:10:33 +0600

    create master panel

---
+++ add_master.php
@@ -0,0 +1,65 @@
+<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3>Add Master</h3>
+					</div>
+					<div class="panel-body">
+						<div style="max-width: 600px; margin: 0 auto;">
+
+							<form class="form-horizontal" action="" method="POST">
+
+								
+								  <div class="form-group required">
+
+								    <label class="control-label col-sm-3" for="name">Name:</label>
+								    <div class="col-sm-9">
+								      <input type="text" class="form-control" name="name" id="name" placeholder="Enter Name">
+								    </div>
+								  </div>
+								  <div class="form-group required">
+								    <label class="control-label col-sm-3" for="pwd">Age:</label>
+								    <div class="col-sm-9"> 
+								      <input type="text" class="form-control" name="age" id="age" placeholder="Enter Age">
+								    </div>
+								  </div>
+								  <div class="form-group required">
+								    <label class="control-label col-sm-3" for="pwd">Phone:</label>
+								    <div class="col-sm-9"> 
+								      <input type="text" class="form-control" name="phone" id="phone" placeholder="Enter Phone">
+								    </div>
+								  </div>
+								  <div class="form-group required">
+								    <label class="control-label col-sm-3" for="pwd">Adderss:</label>
+								    <div class="col-sm-9"> 
+								      <textarea class="form-control" rows="4" id="address" name="address" placeholder="Enter Address"></textarea>
+								    </div>
+								  </div>
+								  <div class="form-group required">
+								    <label class="control-label col-sm-3" for="pwd">Gender:</label>
+								    <div class="col-sm-9"> 
+								      <label class="radio-inline"><input type="radio" name="gender" value="Male" checked>Male</label>
+									  <label class="radio-inline"><input type="radio" name="gender" value="Female">Female</label>
+									  <label class="radio-inline"><input type="radio" name="gender" value="Other">Other</label>
+								    </div>
+								  </div>
+								  <div class="form-group required">
+								    <label class="control-label col-sm-3" for="pwd">Section:</label>
+								    <div class="col-sm-9"> 
+										<select class="form-control" name="type" id="sel1">
+											
+											<option value="null" disabled="" selected="">Select</option>
+											<option value="Shirt">Shirt</option>
+											<option value="pant">Pant</option>
+											<option value="suit">Suit</option>
+										</select>
+								    </div>
+								  </div>						  
+								  <div class="form-group"> 
+								    <div class="col-sm-offset-3 col-sm-9">
+								      <button type="submit" name="submit" class="btn btn-success">Submit</button>
+								    </div>
+								  </div>
+							</form>
+
+						</div>
+					</div>
+				</div>
